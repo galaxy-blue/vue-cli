9ef75f71fa8952f37bfc63e1c09e03eab86d51de
galaxy-blue <16631116365@163.com>
2018-08-26 15:24:01 +0800

    dev first commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,94 +1,48 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li>
-        <a
-          href="https://vuejs.org"
-          target="_blank"
-        >
-          Core Docs
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://forum.vuejs.org"
-          target="_blank"
-        >
-          Forum
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://chat.vuejs.org"
-          target="_blank"
-        >
-          Community Chat
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://twitter.com/vuejs"
-          target="_blank"
-        >
-          Twitter
-        </a>
-      </li>
-      <br>
-      <li>
-        <a
-          href="http://vuejs-templates.github.io/webpack/"
-          target="_blank"
-        >
-          Docs for This Template
-        </a>
-      </li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li>
-        <a
-          href="http://router.vuejs.org/"
-          target="_blank"
-        >
-          vue-router
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vuex.vuejs.org/"
-          target="_blank"
-        >
-          vuex
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vue-loader.vuejs.org/"
-          target="_blank"
-        >
-          vue-loader
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-        >
-          awesome-vue
-        </a>
-      </li>
-    </ul>
-  </div>
+ <div class="bg">
+   <div class="inputBox">
+     <div class="title">{{title}}</div>
+     <div class="butto">
+     <div class="foot">
+         <input type="text" v-model="user" name="username" value="" placeholder="请输入您的用户名" class="textBox ">
+         <img :src="txtOne" alt="" class="ren">
+         <input type="password" v-model="pwd" name="psw" value="" placeholder="请输入密码" maxlength="6" class="textBox ">
+         <img :src="txtTwo" alt="" class="sou">
+         <input type="submit" name="sbt" value="登 录" class="loginBox" @click="denglu">
+     </div>
+     </div>
+   </div>
+ </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
+  data(){
+    return{
+      user: '',
+      pwd :'',
+      title:'河北工程大学',
+      txtOne:require('../assets/images/ren.png'),
+      txtTwo:require('../assets/images/sou.png'),
+    }
+  },
+  methods:{
+    denglu(){
+      if(this.user ==''|| this.pwd == '' ) {
+        return false;
+      }
+      //ajax
+      this.axios({
+        methods:'post',
+        url:'',
+        params:{
+          name:this.user,
+          pass:this.pwd,
+        }
+      }).then((res)=>{
+        //写后台返回的东西
+      })
+
     }
   }
 }
@@ -96,18 +50,73 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
-  font-weight: normal;
+
+.bg{
+  position: absolute;
+  top: 0;
+  bottom: 0;            /* 撑开 */
+  background: #ccc;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+.inputBox{
+  width:610px;
+  height: 475px;
+  border:6px solid #7d909b;
+  border-radius: 5px;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+.title{
+  width: 100%;
+  height:92px;
+  background: #242b31;
+  color:#fff;
+  text-align: center;
+  line-height: 92px;
+  font-size: 28px;
 }
-a {
-  color: #42b983;
+.butto{
+  width: 100%;
+  height: 380px;
+  background: #fff;
+  padding:1px;
+}
+.foot{
+      width: 411px;
+    height: 230px;
+    margin: 38px auto;
+}
+.ren{
+position: absolute;
+top:180px;
+left:490px;
+}
+.sou{
+  position: absolute;
+  top:260px;
+  left:490px;
+}
+.textBox {
+    width: 415px;
+    height: 52px;
+    margin-bottom: 23px;
+    border: 1px solid #d2dbe2;
+    border-radius: 2px;
+    padding-left: 58px;
+    box-sizing: border-box;
+}
+.loginBox {
+    margin: 33px auto 0;
+    margin-left: 84px;
+    width: 247px;
+    height: 50px;
+    background: #04c3b3;
+    border: 1px solid #04c3b3;
+    border-radius: 5px;
+    color: #fff;
+    font-size: 23px;
+    cursor: pointer;
 }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
-
+import demo from '@/components/demo'
 Vue.use(Router)
 
 export default new Router({
@@ -10,6 +10,10 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },{
+      path:'/demo',
+      name:'demo',
+      component:demo
     }
   ]
 })
